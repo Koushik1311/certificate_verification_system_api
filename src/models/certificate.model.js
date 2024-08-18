2e4c5fc3470789020feaef0dd6b7f0c0ac0b6af5
Koushik <programmer.koushik@gmail.com>
2024-08-18 11:33:05 +0530

    user register, login, logout, refresh token

---
+++ src/models/certificate.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const certificateSchema = new Schema(
+  {
+    certificateId: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    student: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    internshipDomain: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    startingDate: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    endingDate: {
+      type: String,
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+certificateSchema.plugin(mongooseAggregatePaginate);
+
+export const Certificate = mongoose.model("Certificate", certificateSchema);
